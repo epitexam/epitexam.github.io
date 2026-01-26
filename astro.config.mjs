b9bbfd200d86fc10b61aa3d6fdfa583c32b75483
Epitexam <202238868+epitexam@users.noreply.github.com>
2026-01-26 10:53:51 +0100

    chore: add astro-compress dependency to enhance performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,47 @@
-// @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import compress from "astro-compress";
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://epitexam.github.io',
 	base: '/',
-	integrations: [mdx(), sitemap(),],
+
+	integrations: [
+		mdx(),
+		sitemap(),
+		compress()
+	],
+
 	vite: {
 		plugins: [tailwindcss()],
+
 		build: {
-			chunkSizeWarningLimit: 500,
 			minify: "esbuild",
 			cssMinify: true,
+			chunkSizeWarningLimit: 500,
+
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("node_modules")) {
+							return "vendor";
+						}
+					}
+				}
+			}
 		}
 	},
+
 	build: {
-		inlineStylesheets: "auto",
+		inlineStylesheets: "always",
 		assets: '_assets',
 	},
-	prefetch: {
-		prefetchAll: true,
-		defaultStrategy: "hover"
-	},
-	compressHTML: true,
 
+	prefetch: {
+		defaultStrategy: "viewport"
+	},
+
+	compressHTML: true,
 });
