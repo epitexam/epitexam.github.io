b0b9d4b82c5e2577d1f19920428c184e3383b216
Epitexam <202238868+epitexam@users.noreply.github.com>
2026-09-24 01:37:20 +0200

    feat(blog): add teaser mode for upcoming articles

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,7 +12,8 @@ const blog = defineCollection({
 			heroImage: image().optional(),
 			heroImageDescription: z.string().optional(),
 			tags: z.array(z.string()).default([]),
-			draft: z.boolean().default(true)
+			draft: z.boolean().default(true),
+			teaser: z.boolean().default(false),
 		}),
 });
 
